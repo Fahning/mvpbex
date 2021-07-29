26a41d215883b21a3f73463d4feebd9c601be4a3
Cícero Fahning <cicerofahning@hotmail.com>
2021-07-29 11:34:52 -0300

    coluna hash_auth na migration companies

--- database/migrations/2021_06_07_140122_create_companies_table.php
+++ database/migrations/2021_06_07_140122_create_companies_table.php
@@ -17,6 +17,7 @@ class CreateCompaniesTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('cnpj');
+            $table->string('hahs_auth');
             $table->string('domain')->unique();
             $table->string('db_database')->unique();
             $table->string('db_hostname');
