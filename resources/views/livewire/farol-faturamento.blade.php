748bdeb368f09c42b9246da00311adf8022baf24
Cícero Fahning <cicerofahning@hotmail.com>
2021-08-04 16:01:13 -0300

    muda frase dashboard

--- resources/views/livewire/farol-faturamento.blade.php
+++ resources/views/livewire/farol-faturamento.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
         <div class="flex flex-col justify-center items-center h-full">
-            <p class="text-4xl animate-pulse">COMING SOON...</p>
+            <p class="text-4xl animate-pulse">EM BREVE...</p>
         </div>
 </x-app-layout>
 
